ebc80e2d7a34b6e73ab28df3dbbfe12b7146a3e3
Chetan77 <cchoudhari1@gmail.com>
2020-02-12 17:26:23 +0530

    [Chetan]Refactor. Refactored employee attendece

--- UserCase1_attendence.sh
+++ UserCase1_attendence.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Employee attendence"
+function EmployeeAttendence(){
+	echo "Employee attendence"
 if [[ $(($RANDOM%2)) == 0 ]]; then
 	echo "employee is present"
 else
 	echo "employee is not present"
 fi
+}
+EmployeeAttendence
