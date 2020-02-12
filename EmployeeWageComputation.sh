d54c59994f61baf0f9582bc8116a8178c4a8a13d
Chetan77 <cchoudhari1@gmail.com>
2020-02-12 19:29:15 +0530

    [Chetan]Add. added implementation of employee wage using case

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,39 @@
-echo "Well Come to EmployeeWage computation"
+#!/bin/bash
+echo "enter the choice"
+echo "for employee attendence enter 1
+for employee dailywage enter 2
+for employee partTimewage enter 3"
+read choice
+case $choice in
+	1 )
+		function EmployeeAttendence(){
+			echo "Employee attendence"
+			if [[ $(($RANDOM%2)) == 0 ]]; then
+				echo "employee is present"
+			else
+				echo "employee is not present"
+			fi
+		}
+		EmployeeAttendence
+		;;
+	2 )
+		DailyWage(){
+			hours=8
+			wagePerHour=20
+			dailywage=$(($hours * $wagePerHour))
+			echo "dailywage is $dailywage"
+		}
+		DailyWage
+		;;
+	3 )
+		PartTimeEmployeeWage(){
+			partTimeHours=4
+			wagePerHour=20
+			partTimewage=$(($partTimeHours * $wagePerHour))
+			echo "part time wage is $partTimewage"
+		}
+		PartTimeEmployeeWage
+		;;
+	* )
+	echo "enter a correct choice"	
+esac
