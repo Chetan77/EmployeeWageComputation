53d68af3bf0d9e7ed3c3fc9a8ab09d657637869a
Chetan77 <cchoudhari1@gmail.com>
2020-02-13 10:57:44 +0530

    [Chetan]Add. added implemetation of employee monthly wage computation

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,31 @@
-echo "Well Come to EmployeeWage computation"
+#!/bin/bash
+function FullTimeEmployeeWage(){
+	days=$1
+	dailyWage=160
+	monthlyWage=$(($days * $dailyWage))
+	echo "monthlyWage for $days days is $monthlyWage"
+	return $wage
+}
+
+function PartTimeEmployeeWage(){
+	days=$1
+	dailyWage=80
+	monthlyWage=$(($days * $dailyWage))
+	echo "monthlyWage $days days is $monthlyWage"
+}
+echo "select the choice
+for full time monthly wage enter 1
+for part time monthly wage enter 2"
+read choice
+case $choice in
+	1 )
+		days=$((RANDOM % 20))
+		FullTimeEmployeeWage $days
+		;;
+	2 )
+		days=$((RANDOM % 20))
+		PartTimeEmployeeWage $days
+		;;
+	* )	
+	echo "enter a correct choice"	
+esac
