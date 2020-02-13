6b7453bd2fca6ea34fd49291a8b8e6830c2df2c2
Chetan77 <cchoudhari1@gmail.com>
2020-02-13 12:28:03 +0530

    [Chetan]Add. added useCase6-Calculate wage on a condition

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,16 @@
 echo "Well Come to EmployeeWage computation"
+#!/bin/bash
+WagesOnACondition(){
+	hoursperDay=8
+	workingdays=20
+	maxWorkingHoursPM=100
+	workedHours=0
+	days=0
+	while (( (($workedHours<=$maxWorkingHoursPM )) && (($days<$workingdays)) )); do
+		workedHours=$(($workedHours+$(($RANDOM%8))))
+		days=$(($days+1))
+	done
+	monthlyWage=$(($workedHours*20))
+	echo "monthly wage for $workedHours hours is $monthlyWage"
+}
+WagesOnACondition
